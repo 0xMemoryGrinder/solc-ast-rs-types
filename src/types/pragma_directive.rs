3dd40a7f78326252801471e3915f44e0a5d2914b
0xMemoryGrinder <35138272+0xMemoryGrinder@users.noreply.github.com>
2024-02-25 12:42:21 -0500

    fix(types): removed strict deny unknown fields for PragmaDirective type

--- src/types/pragma_directive.rs
+++ src/types/pragma_directive.rs
@@ -39,7 +39,6 @@ use crate::types::SourceLocation;
 #[doc = r" ```"]
 #[doc = r" </details>"]
 #[derive(Clone, Debug, Deserialize, Serialize)]
-#[serde(deny_unknown_fields)]
 pub struct PragmaDirective {
     pub id: i64,
     pub literals: Vec<String>,
